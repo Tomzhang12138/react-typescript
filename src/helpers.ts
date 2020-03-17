d581a469c08666f2c21fb8f1b4ec6d2daab34a73
zyw <1316727932@qq.com>
2020-03-17 11:12:30 +0800

    config

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,2 +1,24 @@
 export const delay = (ms: number) =>
 	new Promise(resolve => setTimeout(resolve, ms))
+
+export const extend = function <T, U>(first: T, second: U): T & U {
+    let result = <T & U>{}
+    for (let id in first) {
+        (<any>result)[id] = (<any>first)[id]
+    }
+    for (let id in second) {
+        if (!result.hasOwnProperty(id)) {
+            (<any>result)[id] = (<any>second)[id]
+        }
+    }
+    return result
+}
+
+export const padLeft = function(value: string, padding: string | number) {
+	if (typeof padding === "number") {
+        return Array(padding + 1).join(" ") + value
+    }
+    if (typeof padding === "string") {
+        return padding + value
+    }
+}
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,30 @@
 import * as React from 'react'
 
-import { Hello } from '../components/hello'
+import { Hello } from '@/components/hello'
+
+import { extend, padLeft } from '@/helpers'
 
 export interface AboutProps { }
 
 export default class About extends React.Component<AboutProps, {}> {
+    componentDidMount() {
+        class Person {
+            constructor(public name: string) { }
+        }
+        interface Loggable {
+            log(info: string): void
+        }
+        class ConsoleLogger implements Loggable {
+            log(info: string) {
+                console.log(`log: ${info}`)
+            }
+        }
+        let jim = extend(new Person("Jim"), new ConsoleLogger())
+        let n = jim.name
+        jim.log(n)
+        let padString = padLeft('hello world', 'hello')
+        console.log(padString)
+    }
     render() {
         return <Hello compiler='Typescript-About' framework='React' />
     }
