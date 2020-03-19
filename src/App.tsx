32b02a6e438d3ebc089175dd2eaa1e03688acd1f
Tomzhang12138 <1316727932@qq.com>
2020-03-19 20:14:31 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import {
     Route,
     Redirect
 } from "react-router-dom"
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles'
 
 import AppRoute from './route'
 import { NotFound } from './404'
@@ -12,11 +14,32 @@ import '@/style/index.less'
 
 export interface AppProps { }
 
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#5da381',
+      },
+      error: {
+        main: '#f5222d'
+      },
+      warning: {
+        main: '#faad14'
+      },
+      info: {
+        main: '#1890ff'
+      },
+      success: {
+        main: '#52c41a'
+      }
+    }
+})
+
 export class App extends React.Component<AppProps, {}> {
     render() {
         return(
             <div>
                 <React.Suspense fallback={<h1>loading...</h1>}>
+                  <ThemeProvider theme={theme}>
                     <Switch>
                         {AppRoute.map((route, index) => {
                             return (
@@ -31,6 +54,7 @@ export class App extends React.Component<AppProps, {}> {
                         <Route path="/404" exact component={NotFound} />
                         <Redirect to="/404" />
                     </Switch>
+                  </ThemeProvider>
                 </React.Suspense>
             </div>
         )
